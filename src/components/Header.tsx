7feb867643219d0917cd82ae6ab2f4c615852dd2
ryan-anderson-instil <randerson@seegrid.com>
2025-07-15 10:10:45 +0100

    add home route to header logo

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,14 @@
+import {Link} from "react-router-dom";
+
 const Header = () => {
     return (
         <header className="bg-white shadow-sm border-b border-gray-200 mb-6">
             <div className="mx-auto py-4">
-                <img
+                <Link to="/"><img
                     src="/ht-logo..svg"
                     alt="Hustle Tracker"
                     className="-ml-8 w-52 h-full"
-                />
+                /></Link>
             </div>
         </header>
     )
